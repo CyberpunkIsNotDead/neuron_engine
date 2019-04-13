97708d42f85511dbf815d6e52f7b522226ef9d7c
neuro <asuka2ndchild@gmail.com>
2019-04-14 00:34:28 +0500

    some improvements in css and replies

--- neuron_engine/board/misc.py
+++ neuron_engine/board/misc.py
@@ -7,22 +7,20 @@ def assemble_replies(replies_to_post, board, post):
     for r in replies_to_post:
         if r:
             if r.child_thread:
-                link = '>>%s' % r.child_thread.counter
-                url = '/%s/thread/%s/' % (
+                link = '<a href="/%s/thread/%s/">&gt;&gt;%s</a>' % (
                     board.url,
-                    r.child_thread.counter
+                    r.child_thread.counter,
+                    r.child_thread.counter,
                     )
-                i = (link, url)
-                replies_urls.append(i)
+                replies_urls.append(link)
             elif r.child_post:
-                url = '/%s/thread/%s/#%s' % (
+                link = '<a href="/%s/thread/%s/#%s">&gt;&gt;%s</a>' % (
                     board.url,
                     r.child_post.thread.counter,
-                    r.child_post.counter
+                    r.child_post.counter,
+                    r.child_post.counter,
                     )
-                link = '>>%s' % r.child_post.counter
-                i = (link, url)
-                replies_urls.append(i)
+                replies_urls.append(link)
     return replies_urls
 
 ''' counter '''
