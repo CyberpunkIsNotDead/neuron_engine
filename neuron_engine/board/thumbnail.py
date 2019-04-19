57f15d4d3ac12f8a10a6a58d8d5d1eb4d61831a3
neuro <asuka2ndchild@gmail.com>
2019-04-19 20:48:53 +0500

    div for files without thumbnail

--- neuron_engine/board/thumbnail.py
+++ neuron_engine/board/thumbnail.py
@@ -5,14 +5,15 @@ import os
 def create_thumbnail(board, infile):
 
     outfile = os.path.splitext(infile.name)[0] + "_thumbnail"
-    im = Image.open(infile)
 
-    im.thumbnail((200, 200), Image.ANTIALIAS)
-    thumb_path = os.path.join(board.url, 'thumbnails')
+    with Image.open(infile) as im:
 
-    save_path = os.path.join(settings.MEDIA_ROOT, thumb_path)
-    os.makedirs(save_path, exist_ok=True)
-    im.save(save_path + '/' + outfile + '.png', "PNG")
+        im.thumbnail((200, 200), Image.ANTIALIAS)
+        thumb_path = os.path.join(board.url, 'thumbnails')
+
+        save_path = os.path.join(settings.MEDIA_ROOT, thumb_path)
+        os.makedirs(save_path, exist_ok=True)
+        im.save(save_path + '/' + outfile + '.png', "PNG")
 
     return os.path.join(thumb_path, outfile + '.png')
 
